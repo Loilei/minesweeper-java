b7fb0e941549216b3525acfa9cf93635419c8977
Loilei <marcelina.gasiorek@gmail.com>
2022-05-20 20:46:44 +0200

    Validation to user inputs added

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -3,6 +3,9 @@ package game;
 import display.BoardDisplay;
 import display.View;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class GameController {
     private Game game;
     private Player player;
@@ -22,28 +25,53 @@ public class GameController {
         this.player = new Player();
         player.setName(inputScanner.getStringInput());
         view.printMessage("\nHello " + player.getName() + ", please choose your map size ");
-        view.printMessage("Choose map height (minimum 5, maximum 10): ");
-        int height = inputScanner.getIntInput();
-        view.printMessage("Choose map width (minimum 5, maximum 10): ");
-        int width = inputScanner.getIntInput();
+        int height = inputScanner.getBoardSizeInput("Choose map height (minimum 5, maximum 10) and hit Enter: ");
+        int width = inputScanner.getBoardSizeInput("Choose map width (minimum 5, maximum 10) and hit Enter: ");
+
         this.game = new Game(height, width);
 
+        view.printMessage("Let the game begin!\n");
         boardDisplay.printBoard(game.getBoard());
 
-        while (game.getBoard().areAllTilesRevealed() || player.isAlive()) {
-            String chosenCoordinates = "";
-            view.printMessage("Choose a coordinates of a tile to reveal it or place a flag (example: B3):");
-            do {
-                chosenCoordinates = inputScanner.getStringInput();
-                if (!areCoordinatesValid(chosenCoordinates)) {
-                    view.printMessage("Coordinates are not valid");
-                }
-            }
-            while (!areCoordinatesValid(chosenCoordinates));
-
-            view.printMessage("Press R to reveal the tile. Press F to place or remove a flag.");
-            String action = inputScanner.getStringInput();
+        while (!isGameOver()) {
+            String chosenCoordinates = getCoordinates();
+            String chosenAction = getAction();
+            //TODO reveal tile logic
         }
+    }
+
+    private String getAction() {
+        String action = "";
+        view.printMessage("\nPress R to reveal the tile. Press F to place or remove a flag.");
+        do {
+            action = inputScanner.getStringInput().toUpperCase();
+            if (!isActionValid(action)) {
+                view.printMessage("Action is not valid. Choose a valid action [R/F]: ");
+            }
+        } while (!isActionValid(action));
+        return action;
+    }
+
+    private boolean isActionValid(String action) {
+        final var validActions = Arrays.asList("F", "R");
+        return validActions.contains(action);
+    }
+
+    private String getCoordinates() {
+        String chosenCoordinates = "";
+        view.printMessage("\nChoose a coordinates of a tile to reveal it or place a flag (example: B3): ");
+        do {
+            chosenCoordinates = inputScanner.getStringInput();
+            if (!areCoordinatesValid(chosenCoordinates)) {
+                view.printMessage("Coordinates are not valid");
+            }
+        }
+        while (!areCoordinatesValid(chosenCoordinates));
+        return chosenCoordinates;
+    }
+
+    private boolean isGameOver() {
+        return game.getBoard().areAllTilesRevealed() || !player.isAlive();
     }
 
     private boolean areCoordinatesValid(String chosenCoordinates) {
--- src/main/java/game/InputScanner.java
+++ src/main/java/game/InputScanner.java
@@ -2,9 +2,7 @@ package game;
 
 import lombok.NoArgsConstructor;
 
-import java.util.Locale;
 import java.util.Scanner;
-
 
 public class InputScanner {
     private Scanner scanner;
@@ -17,8 +15,20 @@ public class InputScanner {
         return scanner.nextLine().toUpperCase();
     }
 
-    public int getIntInput() {
-        int number = scanner.nextInt();
+    public int getBoardSizeInput(String message) {
+        int number;
+        System.out.println(message);
+        do {
+            while (!scanner.hasNextInt()) {
+                System.out.println("Please enter a valid number");
+                scanner.next();
+            }
+            number = scanner.nextInt();
+            if (number > 10 || number < 5) {
+                System.out.println("Please enter a number from range 5 to 10");
+            }
+        }
+        while (number > 10 || number < 5);
         scanner.nextLine();
         return number;
     }
