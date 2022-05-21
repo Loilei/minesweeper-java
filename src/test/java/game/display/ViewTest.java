9013a712f44be0de35fceb23fc99515a94cfee2d
Loilei <marcelina.gasiorek@gmail.com>
2022-05-21 14:00:56 +0200

    View class tests added

---
+++ src/test/java/game/display/ViewTest.java
@@ -0,0 +1,81 @@
+package game.display;
+
+import game.model.Player;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ViewTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final View view = new View();
+
+
+    @BeforeEach
+    void setup() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void reset() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void whenPrintMessageIsCalled_displayTheMessage() {
+        //GIVEN
+        var expectedMessage = "Message on display";
+        //WHEN
+        view.printMessage(expectedMessage);
+        //THEN
+        assertEquals(expectedMessage, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void whenWelcomeMessagePrintIsCalled_displayTheMessage() {
+        //GIVEN
+        var expectedMessage = "Welcome to Minesweeper!\n Wanna play a game?\n";
+        //WHEN
+        view.printMessage(expectedMessage);
+        //THEN
+        assertEquals(expectedMessage.trim(), outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void whenWelcomePlayerMessagePrintIsCalled_displayTheMessage() {
+        //GIVEN
+        var player = new Player();
+        player.setName("Alice");
+        var expectedMessage = "\nHello " + player.getName() + ", please choose your map size ";
+        //WHEN
+        view.printMessage(expectedMessage);
+        //THEN
+        assertEquals(expectedMessage.trim(), outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void whenPrintBombsLeftIsCalled_displayTheMessage() {
+        //GIVEN
+        var numberOfBombs = 5;
+        var expectedMessage = "Number of bombs: " + numberOfBombs;
+        //WHEN
+        view.printMessage(expectedMessage);
+        //THEN
+        assertEquals(expectedMessage.trim(), outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void whenPrintVictoryMessageIsCalled_displayTheMessage() {
+        //GIVEN
+        var expectedMessage = "Congratulations! Patron would be proud!";
+        //WHEN
+        view.printMessage(expectedMessage);
+        //THEN
+        assertEquals(expectedMessage.trim(), outputStreamCaptor.toString().trim());
+    }
+}
